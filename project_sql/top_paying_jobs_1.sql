edb20de245856d0af32927b3d0a3210aa89b20e1
Filip Geburczyk <fgeburczyk@gmail.com>
2024-05-07 19:15:11 +0200

    Generic query for top paying jobs

---
+++ project_sql/top_paying_jobs_1.sql
@@ -0,0 +1,22 @@
+-- Update later
+
+SELECT
+     t1.job_id
+    ,t2.name
+    ,t1.job_title
+    ,t1.job_location
+    ,t1.job_work_from_home
+    ,t1.job_schedule_type
+    ,t1.salary_year_avg
+    ,t1.job_posted_date
+FROM job_postings_fact t1
+LEFT JOIN company_dim t2
+    ON t1.company_id = t2.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    job_work_from_home = TRUE AND
+    salary_year_avg IS NOT NULL
+ORDER BY salary_year_avg DESC
+LIMIT 10
+;
